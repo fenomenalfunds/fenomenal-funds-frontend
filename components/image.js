ffdb667ef428a98cbb755561ed9323051e229c81
Monica Marques <mmmonica50@gmail.com>
2021-04-23 01:40:39 -0500

    Bug fix
    - Local image has no alternativeText

--- components/image.js
+++ components/image.js
@@ -5,7 +5,7 @@ const Image = ({image, ...props}) => {
 	if(image && image.url) {
 		return <CloudImage image={image} {...props} />;
 	} else {
-		return <img src={getLocalUrl('/FF_2021_ImagePlaceholder_1080x810.jpg')} alt={image.alternativeText} />
+		return <img src={getLocalUrl('/FF_2021_ImagePlaceholder_1080x810.jpg')} alt="" />
 	}
 };
 
