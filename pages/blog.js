1b1ffc9e8d351b4c0a5a5501744a6d7926b1b64f
Monica Marques <mmmonica50@gmail.com>
2021-05-05 10:34:53 -0500

    Fix blog static paths error

--- pages/blog.js
+++ pages/blog.js
@@ -5,10 +5,10 @@ export async function getStaticProps({params, preview = {}}) {
 	return pagedStaticProps({params, preview});
 }
 
-export async function getStaticPaths() {
+/*export async function getStaticPaths() {
 	//const paths = await getCategoryPaths(FIRST_PAGE_SIZE, OTHER_PAGE_SIZE);
 	return {
 		paths: [], //paths.filter(x => x.params?.page === 1),
 		fallback: true,
 	};
-}
+}*/
